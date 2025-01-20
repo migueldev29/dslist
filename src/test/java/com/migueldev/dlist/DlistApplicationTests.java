179ba2f2fdb85829559ec48d7c5de20208ec3c74
Miguel Francischini <migueldev29@gmail.com>
2025-01-20 19:17:07 -0300

    Project created

---
+++ src/test/java/com/migueldev/dlist/DlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migueldev.dlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
